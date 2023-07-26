8ea42f9872043b600127b58fba876962a6034a7a
aniket rai <113430035+aniketrai11@users.noreply.github.com>
2023-07-26 10:44:30 +0530

    Add files via upload

---
+++ TransposOfMatrix.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a[][3]={{1,2,3},{4,5,6},{7,8,9}};
+	int temp;
+    
+    for(int i=0;i<3;i++)
+    {
+    	for(int j=i;j<3;j++)
+    	{   //swap
+    		temp=a[i][j];
+    		a[i][j]=a[j][i];
+    		a[j][i]=temp;
+    			
+		}
+	}
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+
+
+}
